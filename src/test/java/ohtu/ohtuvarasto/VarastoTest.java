d878c907b7fa6fe80855a91c301d4a2c6555922d
EnsioS <ensiosuonpera@luukku.com>
2017-11-12 20:44:52 +0200

    poistin importit oikeasta tiedostosta

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -3,12 +3,9 @@ package ohtu.ohtuvarasto;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 public class VarastoTest {
 
